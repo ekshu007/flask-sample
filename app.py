8257247ec4e8dabb7a8631b5daa5ff50dbe5c300
ekshu007 <146545198+ekshu007@users.noreply.github.com>
2024-04-28 17:34:58 +0000

    flask sample html code

--- app.py
+++ app.py
@@ -1,1 +1,12 @@
-print("Hello World")
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello_world():
+  return render_template('home.html')
+
+
+if __name__ == "__main__":
+  app.run(host="0.0.0.0", debug=True)
